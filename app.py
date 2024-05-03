d8a9b228951a1d1b3bf48f4e95b82d7f2515c199
bentan4853563 <bentan010918+1@gmai.com>
2024-05-03 09:13:59 +0300

    added testing code

--- app.py
+++ app.py
@@ -199,8 +199,7 @@ def analyze(text):
         messages=[
             {"role": "system", "content": "You are a helpfull and sensitive assitant."},
             {
-                "role": "system",
-                "content": "You have to generate 5(not less, must 5) multiple choice questions based on the user's message . Each question should have 4 options, with one correct answer. The correct answer should be indicated separately. Additionally, provide a single explanation that will be displayed when a learner selects any of the wrong answers. The explanation should avoid direct references to the article.",
+                "role": "system", "content": "You have to generate 5(not less, must 5) multiple choice questions based on the next user's message . Each question should have 4 options, with one correct answer. The correct answer should be indicated separately. Additionally, provide a single explanation that will be displayed when a learner selects any of the wrong answers. The explanation should avoid direct references to the user's message.",
             },
             {"role": "user", "content": f"{text}"},
         ],
