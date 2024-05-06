9b134ffc5bc35aa96651874e475cecad3d52b81f
bentan4853563 <bentan010918+1@gmai.com>
2024-05-06 08:41:59 +0300

    upgrated gpt-4-turbo

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def is_youtube_url(url):
 def extract_hashtag(text):
     """Function extract_hashtag"""    
     
-    model = "gpt-4-1106-preview"
+    model = "gpt-4-turbo"
 
     response = client.chat.completions.create(
         model=model,
@@ -126,7 +126,7 @@ def summarize(text):
     ]
     
     response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4-turbo",
         messages=[
             {"role": "system", "content": "You are a helpfull and sensitive assitant."},
             {
@@ -183,7 +183,7 @@ def analyze(text):
     ]    
     
     response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4-turbo",
         messages=[
             {"role": "system", "content": "You are a helpfull and sensitive assitant."},
             {
